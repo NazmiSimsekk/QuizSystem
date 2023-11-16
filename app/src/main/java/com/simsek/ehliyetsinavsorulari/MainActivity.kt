0c419ce0a7d0f7434114ff3d6c66323df8f841c2
Nazmi Şimşek <61654930+GraivRar@users.noreply.github.com>
2023-11-16 18:57:15 +0300

    Guncelleme

--- app/src/main/java/com/simsek/ehliyetsinavsorulari/MainActivity.kt
+++ app/src/main/java/com/simsek/ehliyetsinavsorulari/MainActivity.kt
@@ -3,13 +3,8 @@ package com.simsek.ehliyetsinavsorulari
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.simsek.ehliyetsinavsorulari.databinding.ActivityMainBinding
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
